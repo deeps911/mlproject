b901394f67ff4676ce52e57e94cb264d4d357696
DEEPAK VERMA <deepakverma911@gmail.com>
2023-07-06 13:20:51 +0530

    data engestion

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = 'e .'
-
-def get_requirements(file_path:str)-> List[str]:
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
     '''
-    this will return list of requirement
+    this function will return the list of requirements
     '''
-    requirements =[]
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
+        requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
@@ -17,13 +16,12 @@ def get_requirements(file_path:str)-> List[str]:
     
     return requirements
 
-
 setup(
-    name = 'mlproject',
-    version = '0.1.1',
-    author = 'deepak',
-    author_email= 'deepakverma911@gmail.com',
-    packages=find_packages(),
-    install_requires =  get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 
 )
